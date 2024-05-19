67c4899f2c2dd66777045c45f2f119e73e7b0f1b
SeymaArslan <derya.ugurlu@ankara.bel.tr>
2024-05-19 16:12:43 +0300

    Initial Commit

---
+++ Bir Esnaf V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bir Esnaf V2
+//
+//  Created by Seyma Arslan on 19.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
